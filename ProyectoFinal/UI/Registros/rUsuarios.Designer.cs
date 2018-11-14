868bdd861f10c008c5e959af008125035822e8aa
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-11-14 09:05:57 -0400

    diseñando registro de usuarios

---
+++ ProyectoFinal/UI/Registros/rUsuarios.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace ProyectoFinal.UI.Registros
+{
+    partial class rUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.UserIdNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NameUserTextBox = new System.Windows.Forms.TextBox();
+            this.UserNameTextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.UserIdNumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(18, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ID";
+            // 
+            // UserIdNumericUpDown
+            // 
+            this.UserIdNumericUpDown.Location = new System.Drawing.Point(67, 24);
+            this.UserIdNumericUpDown.Name = "UserIdNumericUpDown";
+            this.UserIdNumericUpDown.Size = new System.Drawing.Size(102, 20);
+            this.UserIdNumericUpDown.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nombres";
+            // 
+            // NameUserTextBox
+            // 
+            this.NameUserTextBox.Location = new System.Drawing.Point(67, 53);
+            this.NameUserTextBox.Name = "NameUserTextBox";
+            this.NameUserTextBox.Size = new System.Drawing.Size(160, 20);
+            this.NameUserTextBox.TabIndex = 3;
+            // 
+            // UserNameTextBox
+            // 
+            this.UserNameTextBox.Location = new System.Drawing.Point(67, 90);
+            this.UserNameTextBox.Name = "UserNameTextBox";
+            this.UserNameTextBox.Size = new System.Drawing.Size(160, 20);
+            this.UserNameTextBox.TabIndex = 4;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 93);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Usuario";
+            // 
+            // rUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(513, 277);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.UserNameTextBox);
+            this.Controls.Add(this.NameUserTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.UserIdNumericUpDown);
+            this.Controls.Add(this.label1);
+            this.Name = "rUsuarios";
+            this.Text = "rUsuarios";
+            ((System.ComponentModel.ISupportInitialize)(this.UserIdNumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown UserIdNumericUpDown;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NameUserTextBox;
+        private System.Windows.Forms.TextBox UserNameTextBox;
+        private System.Windows.Forms.Label label3;
+    }
+}
