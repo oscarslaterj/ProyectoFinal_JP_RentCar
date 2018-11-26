fa1b903059b16983efe24b193fc32f3239738046
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-11-26 18:08:25 -0400

    Creando Test

---
+++ ProyectoFinalTests/BLL/ClientesBLLTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProyectoFinal.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoFinal.BLL.Tests
+{
+    [TestClass()]
+    public class ClientesBLLTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
