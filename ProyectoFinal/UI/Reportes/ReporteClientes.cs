5596a6ce363ef933d5016012c661545937631ff1
Oscar Slater Jimenez Paula <oscar_jimenez@ucne.edu.do>
2018-12-04 02:02:35 -0400

    agonizando...

--- ProyectoFinal/UI/Reportes/ReporteClientes.cs
+++ ProyectoFinal/UI/Reportes/ReporteClientes.cs
@@ -16,14 +16,14 @@ namespace ProyectoFinal.UI.Reportes {
     using CrystalDecisions.CrystalReports.Engine;
     
     
-    public class ReporteRenta : ReportClass {
+    public class ReporteClientes : ReportClass {
         
-        public ReporteRenta() {
+        public ReporteClientes() {
         }
         
         public override string ResourceName {
             get {
-                return "ReporteRenta.rpt";
+                return "ReporteClientes.rpt";
             }
             set {
                 // Do nothing
@@ -41,7 +41,7 @@ namespace ProyectoFinal.UI.Reportes {
         
         public override string FullResourceName {
             get {
-                return "ProyectoFinal.UI.Reportes.ReporteRenta.rpt";
+                return "ProyectoFinal.UI.Reportes.ReporteClientes.rpt";
             }
             set {
                 // Do nothing
@@ -66,7 +66,7 @@ namespace ProyectoFinal.UI.Reportes {
         
         [Browsable(false)]
         [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.CrystalReports.Engine.Section GroupHeaderSection1 {
+        public CrystalDecisions.CrystalReports.Engine.Section Section3 {
             get {
                 return this.ReportDefinition.Sections[2];
             }
@@ -74,7 +74,7 @@ namespace ProyectoFinal.UI.Reportes {
         
         [Browsable(false)]
         [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.CrystalReports.Engine.Section Section3 {
+        public CrystalDecisions.CrystalReports.Engine.Section Section4 {
             get {
                 return this.ReportDefinition.Sections[3];
             }
@@ -82,33 +82,17 @@ namespace ProyectoFinal.UI.Reportes {
         
         [Browsable(false)]
         [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.CrystalReports.Engine.Section GroupFooterSection1 {
-            get {
-                return this.ReportDefinition.Sections[4];
-            }
-        }
-        
-        [Browsable(false)]
-        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
-        public CrystalDecisions.CrystalReports.Engine.Section Section4 {
-            get {
-                return this.ReportDefinition.Sections[5];
-            }
-        }
-        
-        [Browsable(false)]
-        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
         public CrystalDecisions.CrystalReports.Engine.Section Section5 {
             get {
-                return this.ReportDefinition.Sections[6];
+                return this.ReportDefinition.Sections[4];
             }
         }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
-    public class CachedReporteRenta : Component, ICachedReport {
+    public class CachedReporteClientes : Component, ICachedReport {
         
-        public CachedReporteRenta() {
+        public CachedReporteClientes() {
         }
         
         [Browsable(false)]
@@ -145,7 +129,7 @@ namespace ProyectoFinal.UI.Reportes {
         }
         
         public virtual CrystalDecisions.CrystalReports.Engine.ReportDocument CreateReport() {
-            ReporteRenta rpt = new ReporteRenta();
+            ReporteClientes rpt = new ReporteClientes();
             rpt.Site = this.Site;
             return rpt;
         }
